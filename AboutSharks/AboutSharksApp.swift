9eab57d5c96bc8744aa8f595fc8e3b3769d7bc8c
lucaszdev <lucas.lima.tlou@icloud.com>
2023-02-24 20:01:49 -0300

    Initial Commit

---
+++ AboutSharks/AboutSharksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutSharksApp.swift
+//  AboutSharks
+//
+//  Created by Lucas Lima on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutSharksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
